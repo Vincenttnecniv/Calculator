428753a13edc3c7d69a5f682737f13e72a7c24a0
Lian Bond <lian.bond@mjhudson.com>
2022-09-23 01:03:53 +0100

    Lian Optimization

--- oef8.js
+++ oef8.js
@@ -1,245 +1,28 @@
-let calcDisplay = [];
+let calcDisplayArray = [];
+
+//handle button click
+function handleButtonClick(element) {
+  let number = element.value;
+  calcDisplayArray.push(number);
+  let calcDisplayText = calcDisplayArray.join(" ");
+  calcDisplayText = calcDisplayText.replaceAll("*", "x");
+  calcDisplayText = calcDisplayText.replaceAll("/", "÷");
+  document.getElementById("display-array").textContent = calcDisplayText;
+  let buttonType = element.getAttribute("data:buttontype");
+}
+
 window.onload = function () {
-  //Add a plus to sum
-  let plus = document.getElementById("plus");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let plusVal = document.getElementById("plus").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addPlus = () => {
-    console.log("Add plus is called");
-    calcDisplay.push(plusVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (plus) {
-    plus.addEventListener("click", addPlus);
-  }
-
-  //Add a minus to sum
-  let minus = document.getElementById("minus");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let minusVal = document.getElementById("minus").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addMinus = () => {
-    console.log("Add minus is called");
-    calcDisplay.push(minusVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (minus) {
-    minus.addEventListener("click", addMinus);
-  }
-
-  //Add a devide to sum
-  let devide = document.getElementById("devide");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let devideVal = document.getElementById("devide").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addDevide = () => {
-    console.log("Add devide is called");
-    calcDisplay.push(devideVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (devide) {
-    devide.addEventListener("click", addDevide);
-  }
-
-  //Add a times to sum
-  let times = document.getElementById("times");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let timesVal = document.getElementById("times").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addTimes = () => {
-    console.log("Add times is called");
-    calcDisplay.push(timesVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (times) {
-    times.addEventListener("click", addTimes);
-  }
-
-  //Add a one to sum
-  let one = document.getElementById("one");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let valOne = document.getElementById("one").value;
-  let oneVal = Number(valOne);
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addOne = () => {
-    console.log("Add one is called");
-    calcDisplay.push(oneVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (one) {
-    one.addEventListener("click", addOne);
-  }
-
-  //Add a two to sum
-  let two = document.getElementById("two");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let twoVal = document.getElementById("two").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addTwo = () => {
-    console.log("Add two is called");
-    calcDisplay.push(twoVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (two) {
-    two.addEventListener("click", addTwo);
-  }
-
-  //Add a three to sum
-  let three = document.getElementById("three");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let threeVal = document.getElementById("three").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addThree = () => {
-    console.log("Add three is called");
-    calcDisplay.push(threeVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (three) {
-    three.addEventListener("click", addThree);
-  }
-
-  //Add a four to sum
-  let four = document.getElementById("four");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let fourVal = document.getElementById("four").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addFour = () => {
-    console.log("Add four is called");
-    calcDisplay.push(fourVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (four) {
-    four.addEventListener("click", addFour);
-  }
-
-  //Add a five to sum
-  let five = document.getElementById("five");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let fiveVal = document.getElementById("five").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addFive = () => {
-    console.log("Add five is called");
-    calcDisplay.push(fiveVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (five) {
-    five.addEventListener("click", addFive);
-  }
-
-  //Add a six to sum
-  let six = document.getElementById("six");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let sixVal = document.getElementById("six").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addSix = () => {
-    console.log("Add six is called");
-    calcDisplay.push(sixVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (six) {
-    six.addEventListener("click", addSix);
-  }
-
-  //Add a seven to sum
-  let seven = document.getElementById("seven");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let sevenVal = document.getElementById("seven").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addSeven = () => {
-    console.log("Add seven is called");
-    calcDisplay.push(sevenVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (seven) {
-    seven.addEventListener("click", addSeven);
-  }
-
-  //Add a eight to sum
-  let eight = document.getElementById("eight");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let eightVal = document.getElementById("eight").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addEight = () => {
-    console.log("Add eight is called");
-    calcDisplay.push(eightVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (eight) {
-    eight.addEventListener("click", addEight);
-  }
-
-  //Add a nine to sum
-  let nine = document.getElementById("nine");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let nineVal = document.getElementById("nine").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addNine = () => {
-    console.log("Add nine is called");
-    calcDisplay.push(nineVal);
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (nine) {
-    nine.addEventListener("click", addNine);
-  }
-
-  //Add a zero to sum
-  let zero = document.getElementById("zero");
-  // console.log("🚀 ~ file: oef8.js ~ line 4 ~ plus", plus);
-  let zeroVal = document.getElementById("zero").value;
-  // console.log("🚀 ~ file: oef8.js ~ line 6 ~ plusVal", plusVal);
-  const addZero = () => {
-    console.log("Add zero is called");
-    calcDisplay.push(zeroVal);
-    console.log(
-      "🚀 ~ file: oef8.js ~ line 219 ~ addZero ~ calcDisplay",
-      calcDisplay
-    );
-    console.log("🚀 ~ file: oef8.js ~ line 2 ~ calcDisplay", calcDisplay);
-    let removeQuotes = calcDisplay.reduce((a, b) => a + " " + b);
-    document.getElementById("display-array").textContent = removeQuotes;
-  };
-  if (zero) {
-    zero.addEventListener("click", addZero);
-  }
-
   //go back
   let goBackOne = document.getElementById("back");
 
   const removeOne = () => {
     console.log("Add removeOne is called");
 
-    if (calcDisplay.length > 1) {
-      calcDisplay.pop();
-      document.getElementById("display-array").textContent = calcDisplay;
+    if (calcDisplayArray.length > 1) {
+      calcDisplayArray.pop();
+      document.getElementById("display-array").textContent = calcDisplayArray;
     } else {
-      calcDisplay = [];
+      calcDisplayArray = [];
       document.getElementById("display-array").textContent = 0;
     }
   };
@@ -253,7 +36,7 @@ window.onload = function () {
     event.preventDefault();
     console.log("handleForm");
     document.getElementById("display-array").textContent = [];
-    return (calcDisplay = []);
+    return (calcDisplayArray = []);
   }
   form.addEventListener("submit", handleForm);
 
@@ -262,7 +45,7 @@ window.onload = function () {
 
   const doEquation = () => {
     console.log("Do equation");
-    let removeQuotes = calcDisplay.reduce((a, b) => {
+    let removeQuotes = calcDisplayArray.reduce((a, b) => {
       return a + "" + b;
     });
     try {
@@ -278,7 +61,7 @@ window.onload = function () {
       alert(error);
       console.log(e.message);
       console.log("yes");
-      calcDisplay = [];
+      calcDisplayArray = [];
       window.location.reload();
     }
     if (result < 0) {
